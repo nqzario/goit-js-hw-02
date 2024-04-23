03f69ddcdd593bd06fb3eebf100c4bde257692f5
nqzario <159358436+elizabetlisa@users.noreply.github.com>
2024-04-23 13:55:10 +0300

    first commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,18 @@
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//      const totalPriceDroid = quantity * pricePerDroid;
-//      const balanceComparison = totalPriceDroid <= customerCredits;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+     const totalPriceDroid = quantity * pricePerDroid;
+     const balanceComparison = totalPriceDroid <= customerCredits;
     
-//      if (balanceComparison) {
-//           const message = `You ordered ${quantity} droids worth ${totalPriceDroid} credits!`;
-//                return message;
-//      }
-//      else {
-//           const message = 'Insufficient funds!';
-//           return message;
-//      }
-// }
-// console.log(makeTransaction(5, 3000, 23000));
-// console.log(makeTransaction(3, 1000, 15000));
-// console.log(makeTransaction(10, 5000, 8000));
-// console.log(makeTransaction(8, 2000, 10000));
-// console.log(makeTransaction(10, 500, 5000));
+     if (balanceComparison) {
+          const message = `You ordered ${quantity} droids worth ${totalPriceDroid} credits!`;
+               return message;
+     }
+     else {
+          const message = 'Insufficient funds!';
+          return message;
+     }
+}
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
